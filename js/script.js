a8591fb5c94d3723564b68990b9702c699a4d1fd
Bryce Stradling <bryce@getvyral.com>
2015-05-17 20:51:35 -0500

    finishing up courses page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-
-$('#salesperson a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-})
+$(document).ready(fuction(){
+    $('#salesperson-tab a:first').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+    $('#reciprocal-tab a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+    $('#post-tab a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+    $('#continuing-tab a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+});
